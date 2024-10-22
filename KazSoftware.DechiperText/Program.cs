f5e5439a8e8ae058fc7e26144b992eeff7b0bc32
Nazib Mahfuz <nazibmahfuz60@gmail.com>
2024-10-22 20:51:09 +0600

    Initial Commit
    Decipher the encoded string.

---
+++ KazSoftware.DechiperText/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace KazSoftware.DecipherText
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string cipherText = "UHV0IHRoZSBzdWJqZWN0IG9mIHRoZSBlbWFpbDogQyMgaXMgdGhlIGJlc3Qg";
+            string decodedText = DecipherText(cipherText);
+
+            Console.WriteLine($"Decoded By Nazib. Decoded Text : {decodedText}");
+        }
+
+        public static string DecipherText(string encodedText)
+        {
+            byte[] bytes = Convert.FromBase64String(encodedText);
+            string decodedText = Encoding.UTF8.GetString(bytes);
+
+            return decodedText;
+        }
+    }
+}
